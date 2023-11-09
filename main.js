ccc50113c212fd7bf8d1a9259336688053aea97b
Teu Pai <105930953+Aluninho07@users.noreply.github.com>
2023-11-09 08:24:27 -0300

    s

--- main.js
+++ main.js
@@ -4,11 +4,12 @@ document.querySelector(idElementoAudio).play();
 const plank = document.querySelectorAll(".tecla");
 
 let contador = 0;
-while(contador<plank.length){
+while(contador < plank.length){
     const efeito = plank[contador].classList[1];
     const idAudio = "#som_"+efeito;
     plank[contador].onclick = function(){
-        tocaSom(idAudio); 
+
+        tocaSom (idAudio); 
     }
     contador = contador + 1
     console.log(contador);
